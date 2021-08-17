522083177305952e647bb3de6a39241c33c454a0
Santa Po <nguyentrongtuank62hust@gmail.com>
2021-08-18 06:02:43 +0700

    revamp base structure

--- utils/utils.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-def load_class_name(class_name_path):
-    with open(class_name_path, 'r') as f:
-        data = yaml.load(f, Loader=yaml.FullLoader)
-        return data['class_names']
