4e6ad0c1cf42cfb9aa2f1b7bd3d9bfc404903fd4
santapo <nguyentrongtuank62hust@gmail.com>
2021-08-17 17:00:30 +0000

    add lr scheduler

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -1,16 +1,20 @@
-from pytorch_lightning import callbacks
-from extractor.vgg16_autoencoder import AutoEncoder
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+
 from torch import optim
 import torch.nn as nn
-from extractor import get_autoencoder
 
-import pytorch_lightning as pl
+from extractor import get_autoencoder
+from data import get_datamodule
 
 
 class AutoEncoderModel(pl.LightningModule):
     def __init__(self,
-                optimizer_name: str = "Adam",
+                optimizer_name: str = "adam",
+                lr_scheduler_method_name: str = 'none',
                 lr: float = 0.001,
+                momentum: float = 0.9,
+                weight_decay: float = 5e-4,
                 pretrained: bool = True,
                 freeze_encoder: bool = False):
         """AutoEncoderModel
@@ -19,7 +23,11 @@ class AutoEncoderModel(pl.LightningModule):
         """
         super().__init__()
         self.lr = lr
+        self.momentum = momentum
+        self.weight_decay = weight_decay
         self.optimizer_name = optimizer_name
+        self.lr_scheduler_method_name = lr_scheduler_method_name
+        self.train_loader_total_samples = 197
         self.inp_freeze_encoder = freeze_encoder
         self.model = get_autoencoder(model_name='vgg16_bn', pretrained=pretrained)
         self.criterion = nn.MSELoss()
@@ -39,12 +47,28 @@ class AutoEncoderModel(pl.LightningModule):
         else:
             trainable_parameters = self.model.parameters()
         
-        if self.optimizer_name == "Adam":
+        if self.optimizer_name == "adam":
             optimizer = optim.Adam(trainable_parameters, lr=self.lr)
+        elif self.optimizer_name == 'sgd':
+            optimizer = optim.SGD(trainable_parameters, lr=self.lr, momentum=self.momentum, weight_decay=self.weight_decay)
+        elif self.optimizer_name == 'rmsprop':
+            optimizer = optim.RMSprop(trainable_parameters, lr=self.lr)
         else:
             raise ValueError(f'Optimizer {self.optimizer_name} is not defined')
+        optimizer_list = [optimizer]
+
+        if self.lr_scheduler_method_name == 'none':
+            scheduler_list = []
+        else:
+            if self.lr_scheduler_method_name == 'cosine':
+                scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.train_loader_total_samples)
+            elif self.lr_scheduler_method_name == 'cyclic':
+                scheduler = optim.lr_scheduler.CyclicLR(optimizer, base_lr=self.lr, max_lr=self.max_lr)
+            else:
+                raise ValueError(f"Learning rate scheduler {self.lr_scheduler_method_name} are not defined")
+            scheduler_list = [scheduler]
         
-        return optimizer
+        return optimizer_list, scheduler_list
 
     def training_step(self, batch, batch_idx):
         """
@@ -67,19 +91,29 @@ class AutoEncoderModel(pl.LightningModule):
 
 
 if __name__ == "__main__":
-    from data import get_datamodule
-    from pytorch_lightning.callbacks import ModelCheckpoint
 
-    cifar10_dm = get_datamodule()
-    model = AutoEncoderModel(pretrained=True)
+    cifar10_dm = get_datamodule(
+        batch_size=256,
+        num_workers=2
+    )
+    model = AutoEncoderModel(
+        pretrained=True,
+        optimizer_name='sgd',
+        lr_scheduler_method_name='cosine',
+        lr=0.005
+    )
     model.datamodule = cifar10_dm
+    model.load_from_checkpoint('exps/vgg16_sgd_256_cosine/lightning_logs/version_1/checkpoints/epoch=99-step=15699-val_loss=0.07.ckpt')
     save_checkpoint = ModelCheckpoint(dirpath=None,
                                     filename='{epoch}-{step}-{val_loss:.2f}',
                                     save_top_k=-1)
+    lr_monitor = LearningRateMonitor(logging_interval="step")
+    
     trainer = pl.Trainer(
-        default_root_dir='exps',
-        max_epochs=30,
-        callbacks=[save_checkpoint]
+        default_root_dir='exps/vgg16_sgd_256_cosine',
+        gpus=1,
+        max_epochs=100,
+        callbacks=[save_checkpoint, lr_monitor],
     )
     
     trainer.fit(model, cifar10_dm)
